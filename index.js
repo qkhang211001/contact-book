ee34e11a39152eda7a3d921f524fd15c4f422487
Tran Quoc Khang <qkhang211001@gmail.com>
2021-09-25 13:59:08 +0700

    Lab 1

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const config = require("./app/config");
+const setupContactRoutes = require("./app/routes/contact.routes");
+const { BadRequestError } = require("./app/helpers/errors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+setupContactRoutes(app);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello ban!"
+  });
+});
+
+app.use((req, res, next) => {
+  next(new BadRequestError(404, "Resource not found."));
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error"
+  });
+});
+
+const PORT = config.app.port;
+app.listen(PORT, () => {
+  console.log(`Server is running !! ${PORT}`);
+});
